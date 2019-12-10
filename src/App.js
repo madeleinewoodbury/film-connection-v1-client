64598542893f6d206c6af21bbf448ec620c094a9
madeleinewoodbury <randine25@gmail.com>
2019-12-10 17:44:45 -0600

    Setup redux store and add alert action and reducer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Fragment } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -7,13 +9,15 @@ import Routes from './components/routing/Routes';
 
 const App = () => {
   return (
-    <Fragment>
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route component={Routes} />
-      </Switch>
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route component={Routes} />
+        </Switch>
+      </Fragment>
+    </Provider>
   );
 };
 
